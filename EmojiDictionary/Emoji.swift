21f508926fbced18a78beb3a3daccac6780e415b
Jeewoo Lee <jlee@lcs.on.ca>
2019-11-13 21:01:18 -0500

    Defined the Emoji struct

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,22 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Lee, Jeewoo on 2019-11-13.
+//  Copyright © 2019 Lee, Jeewoo. All rights reserved.
+//
+
+import Foundation
+
+//Define the Emoji struct (describe it's generic properties, etc)
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
+
+//an example of creating an instance of the Emoji struct
+//var example = Emoji(symbol: "🍕", name: "Pizza", description: "A delivious combination of bread, tomato sauce, and cheese.", usage: "make when hungry")
+
+
